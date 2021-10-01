dad62edf893d14c0307d57e89be9c5ff7ee209b0
brindakarthik <brindaspringnet@gmail.com>
2021-10-01 10:12:48 +0530

    commit models

--- polling_project/movie_app/models.py
+++ polling_project/movie_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class MovieModel(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    image_url = models.CharField(max_length=100)
+
+
+class ReviewModel(models.Model):
+    name = models.CharField(max_length=100)
+    vote = models.IntegerField()
+
